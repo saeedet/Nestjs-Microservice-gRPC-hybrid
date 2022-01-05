43eb5278bee136da40222303c8fc37642f1a5b2a
Saeed <saeed_et90@yahoo.com>
2022-01-05 18:40:34 +1100

    envs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const logger = new Logger('Main');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen();
+  logger.log('Microservice is listening...');
+  // app.connectMicroservice<MicroserviceOptions>(grpcClientOptions);
+
+  // await app.startAllMicroservices();
+  // await app.listen(process.env.APPLICATION_PORT);
+
+  // console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
